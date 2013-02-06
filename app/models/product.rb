13e85c2e0b7ef84db3beb83a1778a4d6cc81a8d6
reikoTADA <aug21reiko@yahoo.co.jp>
2013-02-06 16:26:11 +0900

    [#43911289] 価格は100円単位に制限する

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,12 @@ class Product < ActiveRecord::Base
   validates_presence_of :name, :description
   validates_uniqueness_of :name
   validates_numericality_of :price, :only_integer => true, :greater_than_or_equal_to => 0
+  validate :price_must_be_a_multiple_of_hundreds
+
+  private
+    def price_must_be_a_multiple_of_hundreds
+      unless price % 100 == 0
+          errors.add :price, "must be a multiple of hundreds"
+    end
+  end
 end
