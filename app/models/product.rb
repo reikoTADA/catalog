c2c222616f41c4e1666fc5678d70cb4d12cd13db
reikoTADA <aug21reiko@yahoo.co.jp>
2013-02-06 16:18:00 +0900

    [#43911289] 検証ロジックを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price, :image, :image_cache
   mount_uploader :image, ProductImageUploader
+
+  validates_presence_of :name, :description
+  validates_uniqueness_of :name
+  validates_numericality_of :price, :only_integer => true, :greater_than_or_equal_to => 0
 end
